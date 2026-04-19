5a3d9e33861368c51d31f62d94e5bd8bb04c71ec
Duy0812 <kinhvolakinh@gmail.com>
2026-04-19 11:57:30 +0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -7,6 +7,10 @@ public class Customer {
 	private String email;
 	private String address;
 
+	private static List<Customer> customerDB = new ArrayList<>();
+    private static List<Booking> bookingDB = new ArrayList<>();
+
+	
 	public Customer(String customerId, String fullName, String phone, String email, String address) {
 		super();
 		this.customerId = customerId;
@@ -53,12 +57,54 @@ public class Customer {
 	}
 
 	public void registerAccount() {
+		boolean emailExists = customerDB.stream()
+                .anyMatch(c -> c.getEmail().equals(this.email));
+
+        if (emailExists) {
+            System.out.println("❌ Đăng ký thất bại: Email '" + this.email + "' đã được sử dụng.");
+            return false;
+        }
+
+		customerDB.add(this);
+        System.out.println("✅ Đăng ký thành công tài khoản cho khách hàng: " + this.fullName);
+        return true;
 	}
 
-	public void login() {
+	public static Optional<Customer> login(String inputEmail, String inputPassword) {
+        System.out.println("\nĐang xác thực tài khoản: " + inputEmail + "...");
+        
+        Optional<Customer> loggedInUser = customerDB.stream()
+                .filter(c -> c.getEmail().equals(inputEmail) && c.getPassword().equals(inputPassword))
+                .findFirst();
+
+        if (loggedInUser.isPresent()) {
+            System.out.println("✅ Đăng nhập thành công! Chào mừng " + loggedInUser.get().getFullName());
+        } else {
+            System.out.println("❌ Đăng nhập thất bại: Sai email hoặc mật khẩu.");
+        }
+        
+        return loggedInUser;
 	}
 
 	public void viewBookingHistory() {
+		System.out.println("\n=== LỊCH SỬ ĐẶT TIỆC CỦA " + this.fullName.toUpperCase() + " ===");
+
+        // Dùng Java 8 Stream để lọc ra các đơn đặt tiệc thuộc về Customer này
+        List<Booking> myBookings = bookingDB.stream()
+                .filter(b -> b.getCustomerId().equals(this.customerId))
+                .collect(Collectors.toList());
+
+        if (myBookings.isEmpty()) {
+            System.out.println("Bạn chưa có đơn đặt tiệc nào trong hệ thống.");
+        } else {
+            // In ra danh sách sử dụng forEach của Java 8
+            myBookings.forEach(b -> {
+                System.out.println("- Mã đơn: " + b.getBookingId() 
+                        + " | Ngày tổ chức: " + b.getEventDate() 
+                        + " | Trạng thái: " + b.getStatus());
+            });
+        }
+        System.out.println("==================================================");
 	}
 
 	public String getFullName() {
