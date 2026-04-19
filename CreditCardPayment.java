e8285c4a808e55d0ca5ea58203f4c064860162d4
huyduc-glitch <huyduc280406@gmail.com>
2026-04-19 12:05:18 +0700

    Update CreditCardPayment.java

--- CreditCardPayment.java
+++ CreditCardPayment.java
@@ -4,9 +4,14 @@ public class CreditCardPayment implements IPayment {
 
 	@Override
 	public boolean processPayment(double amount) {
-		// TODO Auto-generated method stub
-		System.out.println("Đang xử lý thanh toán thẻ tín dụng: " + amount + " VNĐ");
-		return true;
-	}
+        // Giả lập kiểm tra số dư (ví dụ không quá 1 tỷ)
+        if (amount > 1000000000) {
+            System.out.println("Lỗi: Giao dịch bị từ chối bởi ngân hàng!");
+            return false;
+        }
+        System.out.printf("Thanh toán thẻ thành công: %,.0f VND\n", amount);
+        return true;
+    }
+}	}
 
 }
