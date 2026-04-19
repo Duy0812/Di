049f45232b6a63ef5f812827a7fddc8df40a424c
huyduc-glitch <huyduc280406@gmail.com>
2026-04-19 12:03:55 +0700

    Update CashPayment.java

--- CashPayment.java
+++ CashPayment.java
@@ -5,7 +5,11 @@ public class CashPayment implements IPayment {
 	@Override
 	public boolean processPayment(double amount) {
 		// TODO Auto-generated method stub
-		System.out.println("Khách hàng thanh toán tiền mặt: " + amount + " VNĐ");
+		if(amount <= 0){
+			System.out.println("Lỗi: Số tiền thanh toán không hợp lệ!");
+			return false;
+		}
+		System.out.printf("Thanh toán tiền mặt thành công: %,.0f VND\n", amount);
 		return true;
 	}
 
